1df18287f7c47698c4d86248fe330a4f070c24ee
arock19 <guptaa5@rose-hulman.edu>
2019-06-21 10:15:54 +0530

    V1.0

--- Controllers/UserLoginController.cs
+++ Controllers/UserLoginController.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 
 namespace UserLoginAPI.Controllers
@@ -5,24 +6,53 @@ namespace UserLoginAPI.Controllers
     [ApiController]
     class UserLoginController : AbstractController{
         
-        [HttpPost("Users")]
-        public string addUserLogin([FromBody] User user){
+        [HttpPost("UserAdd")]
+        public static string addUserLogin([FromBody] User user){//checked --works
             string queryString = "EXEC addUser '" + user.Username + 
             "' , '" + user.Password + "' ;";
-            DBWrapper.sendCommand(queryString);
+            try{
+                DBWrapper.sendCommand(queryString);
+            } catch(Exception e) {
+                System.Console.Error.Write(e);
+            }
+            
             return "ok";
         }
 
         [HttpPost("UserCheck")]
-        public bool checkUserLogin([FromBody] User userInput){
-            User userActual = DBWrapper.getUserLoginData(userInput.Username);
+        public static bool checkUserLogin([FromBody] User userInput){//checked --works
+            User userActual = null;
+            try{
+                userActual = DBWrapper.getUserLoginData(userInput.Username);  
+            } catch (Exception e){
+                System.Console.Error.Write(e);
+                return false;
+            }
+            
             if(userActual == null){
+                System.Console.WriteLine("unavailable username");
                 return false;//message -> unavailable username
             } 
             if(userActual.Password.Equals(userInput.Password)){
+                System.Console.WriteLine("correct password");
                 return true;
             }
+            System.Console.WriteLine("incorrect password");
             return false; //message -> incorrect password
+        }
+
+        [HttpPost("UserUpdate")]
+        public static bool updateUserLogin([FromBody] User user){//checked --works
+            string queryString = "EXEC updateUser '" + user.Username + 
+            "' , '" + user.Password + "' ;";
+            try{
+                DBWrapper.sendCommand(queryString);
+            } catch(Exception e) {
+                System.Console.Error.Write(e);
+                return false;
+            }
+            
+            return true;
         }
     } 
 }
